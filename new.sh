3a4f0f99631dc56ba826b743aca5513ac04be222
Nils Eidmann <nilseidmann@gmail.com>
2019-11-28 16:57:31 +0100

    added package manager support, added bundle install

--- new.sh
+++ new.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
 echo "Script #$$ running with $# args: $@"
 
-# First ensure that vim is installed
-#if [ -f /etc/os-release ]; then
-#  # freedesktop.org and systemd
-#  . /etc/os-release
-#  OS=$NAME
-#  VER=$VERSION_ID
-#elif type lsb_release >/dev/null 2>&1; then
-#  # linuxbase.org
-#  OS=$(lsb_release -si)
-#  VER=$(lsb_release -sr)
-#elif [ -f /etc/lsb-release ]; then
-#  # For some versions of Debian/Ubuntu without lsb_release command
-#  . /etc/lsb-release
-#  OS=$DISTRIB_ID
-#  VER=$DISTRIB_RELEASE
-#elif [ -f /etc/debian_version ]; then
-#  # Older  Debian/Ubuntu/etc.
-#  OS=Debian
-#  VER=$(cat /etc/debian_version)
-#else
-#  OS=$(uname -s)
-#  VER=$(uname -r)
-#fi
 function packman
 {
   case `uname` in
@@ -44,6 +21,7 @@ function packman
 packman
 echo $PM
 
+# Install git and vim which are needed for the next steps
 case $PM in
   pacman )
     pacman -Syu
@@ -55,8 +33,88 @@ case $PM in
     apt-get install git
     apt-get install vim
     ;;
+  zypper )
+    zypper patch
+    zypper in git
+    zypper in vim
+    ;;
+  yum )
+    yum update
+    yum install git
+    yum install vim
+    ;;
   * )
     ;;
 esac
-
 echo "Congratulations, you have successfully installed git and vim!"
+
+# Set up vim config
+git clone https://github.com/UKayeF/dotfiles ~
+
+function install_bundle()
+{
+  pathexists=$(ls -A ~/.vim/bundle | grep ${$2})
+  if [[ "$pathexists" != "" ]] then
+    return;
+  fi
+  git clone $1 ~/.vim/bundle/${$2}
+}
+
+if [ "$(ls -A ~ | grep .vimrc)" != ".vimrc" ] then
+  cp ~/dotfiles/.vimrc ~/.vimrc
+  mkdir -p ~/.vim/{bundle,autoload}
+  echo "Install Pathogen? [Y]es/[N]o"
+  read yes
+  if [[ "$yes" =~ [Y|y]?(es) ]] then
+    # Pathogen Vim
+    git clone https://github.com/tpope/vim-pathogen ~/.vim
+    mv ~/.vim/vim-pathogen/autoload/ ~/.vim/autoload/
+    $gruvbox="https://github.com/morhetz/gruvbox"
+    $palenight="https://github.com/drewtempelmeyer/palenight.vim"
+    $nerdtree="https://github.com/scrooloose/nerdtree"
+    $airline="https://github.com/vim-airline/vim-airline"
+
+    echo "Pathogen installed! Which bundles do you want to install?\n
+    1) gruvbox color theme\n
+    2) palenight color theme\n
+    3) nerdtree file explorer\n
+    4) airline-vim powerline\n
+    5) all of them\n
+    6) I'm fine, thanks!"
+    read num
+    case `num` in
+      1 )
+        install_bundle $gruvbox gruvbox
+        ;;
+      2 )
+        install_bundle $palenight palenight
+        ;;
+      3 )
+        install_bundle $nerdtree nerdtree
+        ;;
+      4 )
+        install_bundle $airline airline-vim
+        ;;
+      5 )
+        install_bundle $gruvbox gruvbox
+        install_bundle $palenight palenight
+        install_bundle $nerdtree nerdtree
+        install_bundle $airline airline
+        ;;
+      * )
+        exit 0;
+    esac
+  fi
+fi
+
+echo "Plugins have been installed! Do you want to run vim now?"
+read openvim
+if [[ "$openvim" =~ [Y|y]?(es) ]] then
+  vim ~/.vimrc
+fi
+exit 0
+
+
+
+fi
+
